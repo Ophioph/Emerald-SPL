e61b35310a0cd47c5714dfca09306c12f6cb19b2
Ophioph <chrislagiewski@gmail.com>
2026-08-09 15:44:47 -0400

    moving shit in items.h
    please work

--- include/constants/items.h
+++ include/constants/items.h
@@ -1000,8 +1000,6 @@ enum __attribute__((packed)) Item
     ITEM_SWAP_SNACK = 825,
     ITEM_TWICE_SPICED_RADISH = 826,
     ITEM_POKESHI_DOLL = 827,
-     // Infinite Candy
-    ITEM_iNFINITE_CANDY = 874,
 
     ITEM_STRANGE_BALL = 828,
 
@@ -1053,6 +1051,9 @@ enum __attribute__((packed)) Item
     ITEM_BAXCALIBRITE = 871,
     ITEM_TATSUGIRINITE = 872,
     ITEM_GLIMMORANITE = 873,
+    
+    // Infinite Candy
+    ITEM_iNFINITE_CANDY = 874,
 
     ITEMS_COUNT, 874
     ITEM_FIELD_ARROW = ITEMS_COUNT,
